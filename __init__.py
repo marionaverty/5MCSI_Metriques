e01ebb3bf2a3f818f50d9f6c5794d4198f62d4a2
Marion AVERTY <35835066+marionaverty@users.noreply.github.com>
2024-02-12 15:15:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route("/histogramme/")
-def mongraphique():
+def mongraphiqueG():
     return render_template("histogramme.html")
   
 if __name__ == "__main__":
