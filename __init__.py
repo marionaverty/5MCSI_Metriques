fd8d6f00bae2b62f285802a1b54ae910c2dcedc2
Marion AVERTY <35835066+marionaverty@users.noreply.github.com>
2024-02-12 17:20:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,42 +38,7 @@ def mongraphiqueG():
   
 @app.route("/commits/")
 def commits():
- import requests
-from datetime import datetime
-
-# Fonction pour extraire les minutes d'une chaîne de date
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    minutes = date_object.minute
-    return minutes
-
-# URL de l'API GitHub pour les commits
-github_api_url = "https://api.github.com/repos/marionaverty/5MCSI_Metriques/commits"
-
-# Effectuer une requête GET à l'API GitHub
-response = requests.get(github_api_url)
-
-# Vérifier si la requête a réussi
-if response.status_code == 200:
-    commits_data = response.json()  # Convertir la réponse en JSON
-    commits_per_minute = {}  # Dictionnaire pour stocker le nombre de commits par minute
-
-    # Parcourir les données des commits
-    for commit in commits_data:
-        commit_date = commit['commit']['author']['date']
-        commit_minute = extract_minutes(commit_date)
-        
-        # Ajouter ou incrémenter le nombre de commits pour cette minute
-        commits_per_minute[commit_minute] = commits_per_minute.get(commit_minute, 0) + 1
-
-    # Afficher le nombre de commits par minute
-    for minute, count in commits_per_minute.items():
-        print(f"{minute} : {count}")
-
-    # Maintenant, vous pouvez utiliser ces données pour créer votre graphique
-else:
-    print("Échec de la requête à l'API GitHub")
-
+    return render-template("commits.html")
 
   
 if __name__ == "__main__":
